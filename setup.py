40242242585155e01a8ded46010c7f702af76cf6
Marian Ignev <m.ignev@gmail.com>
2011-05-28 02:32:01 +0300

    Create simple .tmux file from sample .yaml, add config file in system folder and set default editor and etc.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+from __future__ import with_statement
 import distutils.core
 import sys
 import shutil
 import os
+import ConfigParser
 
 # Importing setuptools adds some features like "setup.py develop", but
 # it's optional so swallow the error if it's not there.
@@ -19,10 +21,24 @@ if not os.path.isdir(tmuxer_system_folder):
 
 if not os.path.isdir(tmuxer_system_folder + '/samples'):
     os.mkdir(tmuxer_system_folder + '/samples')
+
+if not os.path.isdir(tmuxer_system_folder + '/tmux_files'):
+    os.mkdir(tmuxer_system_folder + '/tmux_files')
+
 if os.path.exists(tmuxer_system_folder + '/samples/sample.yml'):
     os.remove(tmuxer_system_folder + '/samples/sample.yml')
 
 shutil.copy('samples/sample.yml', tmuxer_system_folder + '/samples/')
+
+if os.path.exists(tmuxer_system_folder + '/config'):
+    os.remove(tmuxer_system_folder + '/config')
+
+config = ConfigParser.RawConfigParser()
+config.add_section('global')
+config.set('global', 'editor', 'vim')
+
+with open(tmuxer_system_folder + '/config') as configfile:
+    config.write(configfile)
 
 distutils.core.setup(name='Tmuxer',
       version='1.0',
--- tmuxer/tmuxer.py
+++ tmuxer/tmuxer.py
@@ -2,47 +2,73 @@ from __future__ import with_statement
 from datetime import datetime
 import yaml
 import os
+import shutil
+import ConfigParser
 
 class Tmuxer:
-
     def __init__(self):
+        self.tmuxer_dir = os.getenv('HOME') + '/.tmuxer'
+        self.sample_conf = self.tmuxer_dir + '/samples/sample.yml'
+        self.compiled_files = self.tmuxer_dir + '/tmux_files/'
+
+        _config_path = self.tmuxer_dir + '/config'
+        config = ConfigParser.RawConfigParser()
+        config.read(_config_path)
+
+        self.editor = config.get('global', 'editor')
+
+    def open(self, project_name):
+        self.current_project = project_name
+        target_project_file = self.tmuxer_dir + '/' + self.current_project + '.yml'
+
+        if os.path.exists(target_project_file):
+            os.system(self.editor + ' ' + target_project_file)
+        else:
+            shutil.copy(self.sample_conf, target_project_file)
+            os.system(self.editor + ' ' + target_project_file)
+
+        return self._process_yml(target_project_file)
+
+    def _process_yml(self, _yml_file):
+        with open(_yml_file, 'r') as yml_stream:
+            yml = yaml.load(yml_stream)
+            print(yml)
+            tmux_file_lines = list()
+            
+            tmux_file_lines.append('#!/bin/bash\n') 
+            tmux_file_lines.append('cd ' + yml['project_root'] + '\n')
+            tmux_file_lines.append('tmux start-server\n\n')
+            tmux_file_lines.append("if ! $(tmux has-session -t '{0}'); then\n\n".format(yml['project_name']))
+
+            tmux_file_lines.append("tmux select-window -t '{0}':0\n".format(yml['project_name']))
+            
+            tmux_file_lines.append("\n# set up tabs and panes\n\n")
+
+            tab_counter = 0
+            for tab in yml['tabs']:
+                tmux_file_lines.append('# tab "{0}"\n'.format(tab['name']))
+                if tab_counter == 0:
+                    tmux_file_lines.append("tmux new-session -d -s '{0}' -n '{1}'\n".format(yml['project_name'], tab['name']))
+                else:
+                    tmux_file_lines.append("tmux new-window -t '{0}':{1} -n '{2}'\n".format(yml['project_name'], tab_counter, tab['name']))
+                self._process_panes(tab['panes'], tab_counter)
+                tab_counter += 1
+
+            tmux_file_lines.append("\nfi\n")
+            
+            tmux_file_lines.append("\nif [ -z $TMUX ]; then\n")
+            tmux_file_lines.append("\ttmux -u attach-session -t '{0}'\n".format(yml['project_name']))
+            
+            tmux_file_lines.append("else\n")
+            tmux_file_lines.append("\ttmux -u switch-client -t '{0}'\n".format(yml['project_name']))
+            tmux_file_lines.append("fi\n")
+
+            with open(self.compiled_files + self.current_project + '.tmux', 'w') as tmux_file:
+                tmux_file.writelines(tmux_file_lines)
+    
+    def _process_panes(self, panes, tab_id):
         pass
 
-    def open(self):
-        home_folder = os.getenv("HOME")
-        tmuxer_system_folder = home_folder + "/.tmuxer"
-        if not os.path.isdir(tmuxer_system_folder):
-            os.mkdir(tmuxer_system_folder)
-
-
-def main():
-    tmuxer = Tmuxer()
-    tmuxer.open()
-    with open('/Users/mignev/Documents/tmuxer/samples/appsc.yml', 'r') as yaml_stream:
-        parser(yaml_stream)
-
-def parser(yaml_stream):
-    yml = yaml.load(yaml_stream)
-    tmux_file_lines = list()
-
-    tmux_file_lines.append('#!/bin/bash\n') 
-    tmux_file_lines.append('cd ' + yml['project_root'] + '\n')
-    tmux_file_lines.append('tmux start-server\n')
-    tmux_file_lines.append("if ! $(tmux has-session -t '{0}'); then\n".format(yml['project_name']))
-
-    print('#!/bin/bash')
-    print('cd ' + yml['project_root'])
-    print(yml['project_name'])
-    print(yml['project_root'])
-
-    for tab in yml['tabs']:
-        print(tab)
-    #os.system('vim appsc.sh')
-
-    print('after os {0}:{1}:{2}'.format(datetime.now().hour, datetime.now().minute, datetime.now().second))
-
-    with open('/tmp/somenew_file', 'w') as tmux_file:
-        tmux_file.writelines(tmux_file_lines)
-
 if __name__ == '__main__':
-    main()
+    tmuxer = Tmuxer()
+    tmuxer.open('appsc')
